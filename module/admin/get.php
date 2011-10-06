ffbd152b9b94b0d5591769082e88464032d82819
Tyler Kellen <tyler@sleekcode.net>
2011-10-05 23:14:31 -0500

    various admin features

--- module/admin/get.php
+++ module/admin/get.php
@@ -3,10 +3,10 @@ function togglesticky_get()
 {
   global $Security;
 
-  if(id() && $Security->is_admin(session('id')))
+  if(id() && $Security->is_admin(session('id')) && md5(session_id()) == cmd(3))
   {
     $Admin = new BoardAdmin;
-    $Admin->toggle_flag("sticky",id());
+    $Admin->toggle_flag("thread","sticky",id());
   }
   return to_index();
 }
@@ -14,10 +14,10 @@ function togglesticky_get()
 function togglelocked_get()
 {
   global $Security;
-  if(id() && $Security->is_admin(session('id')))
+  if(id() && $Security->is_admin(session('id')) && md5(session_id()) == cmd(3))
   {
     $Admin = new BoardAdmin;
-    $Admin->toggle_flag("locked",id());
+    $Admin->toggle_flag("thread","locked",id());
   }
   return to_index();
 }
@@ -25,11 +25,36 @@ function togglelocked_get()
 function togglelegendary_get()
 {
   global $Security;
-  if(id() && $Security->is_admin(session('id')))
+  if(id() && $Security->is_admin(session('id')) && md5(session_id()) == cmd(3))
   {
     $Admin = new BoardAdmin;
-    $Admin->toggle_flag("legendary",id());
+    $Admin->toggle_flag("thread","legendary",id());
   }
   return to_index();
+}
+
+function togglebanned_get()
+{
+  global $Security,$DB;
+  if(id() && $Security->is_admin(session('id')) && md5(session_id()) == cmd(3))
+  {
+    $Admin = new BoardAdmin;
+    $Admin->toggle_flag("member","banned",id());
+    $DB->update("member","id",id(),array("cookie"=>""));
+  }
+  return to_index($_SERVER['HTTP_REFERER']);
+}
+
+function resetlink_get()
+{
+  global $Security,$DB;
+  if(id() && $Security->is_admin(session('id')) && md5(session_id()) == cmd(3))
+  {
+    $DB->query("SELECT email_signup||pass FROM member WHERE id=$1",array(id()));
+    $hash = md5($DB->load_result().time());
+    $DB->update("member","id",id(),array("reset"=>$hash));
+    print "http://$_SERVER[HTTP_HOST]/member/reset/$hash/";
+  }
+  exit_clean();
 }
 ?>
