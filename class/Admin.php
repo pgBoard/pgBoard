b15930436f9f17c57623b9779b672e5757492e20
Tyler Kellen <tyler@sleekcode.net>
2011-10-05 22:59:24 -0500

    generalized toggle function for admin

--- class/Admin.php
+++ class/Admin.php
@@ -1,20 +1,20 @@
 <?php
 class BoardAdmin
 {
-  function toggle_flag($flag,$id)
+  function toggle_flag($table,$flag,$id)
   {
     global $DB;
     $DB->query("UPDATE
-                  thread
+                  {$table}
                 SET
                   {$flag}=(CASE WHEN {$flag} IS true THEN false ELSE true END)
                 WHERE
                   id=$1",array($id));
   }
   
-  function check_flag($flag,$id)
+  function check_flag($table,$flag,$id)
   {
     global $DB;
-    return $DB->value("SELECT {$flag} FROM thread WHERE id=$1",array($id)) == "t" ? true : false;
+    return $DB->value("SELECT {$flag} FROM {$table} WHERE id=$1",array($id)) == "t" ? true : false;
   }
 }
