3e5583abdd7abcd0331c4b63447843b6d23471e4
Tyler Kellen <tyler@sleekcode.net>
2012-12-17 00:35:02 -0600

    snowstorm

---
+++ lib/snowstorm/snow.php
@@ -0,0 +1,15 @@
+<?php if(!session('nosnow')) { ?>
+<script>
+function nosnow(stop) {
+  $.ajax({type:'POST',url:'/',data:{nosnow:true}});
+  snowStorm.stop();
+  $(stop).remove();
+}
+</script>
+<script src="/lib/snowstorm/snow.js"></script>
+<img src="/lib/snowstorm/off.gif" onclick="nosnow(this);" style="float:right"/>
+<?php
+}
+if(post('nosnow')) {
+  $_SESSION['nosnow'] = true;
+}
